56e2b7d78570b291733f29d9f3aef830810dcf55
jh1662 <jh1662@C60049554>
2023-09-27 14:29:43 +0100

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,7 @@
+namespace U10803___Assessment_1 {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+    }
+}
---
+++ items.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U10803___Assessment_1 {
+    internal class items {
+    }
+}

1d0e26cb163a7b87b5e2110e39f550b3285e5a1b
jh1662 <JH1662@canterbury.ac.uk>
2023-10-03 17:46:57 +0100

    insignificant changes

--- accounts.cs
+++ accounts.cs
@@ -1,11 +1,16 @@
 ﻿public class Customer {
     public string name;
     public string email;
-    public 
+    public List<Purchase> purchases = new List<Purchase>();
 }
 
 public class Purchase {
     public DateTime saleDate;
     public decimal price;
     public Dictionary<string, Item> items = new Dictionary<string, Item>();
+}
+
+public class Supplier {
+    string name;
+    public Dictionary<string, Item> stockList = new Dictionary<string, Item>();
 }
